76c928714569717428f3a5cda7b644fadb4cbf3c
André Rodrigues <afbr17@gmail.com>
2020-03-24 14:30:24 +0000

    Add rna-transcription initial solution

---
+++ javascript/rna-transcription.js
@@ -0,0 +1,17 @@
+export const toRna = dna => {
+  let rna = '';
+
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === 'G') {
+      rna += 'C';
+    } else if (dna[i] === 'C') {
+      rna += 'G';
+    } else if (dna[i] === 'T') {
+      rna += 'A';
+    } else {
+      rna += 'U';
+    }
+  }
+
+  return rna;
+};
