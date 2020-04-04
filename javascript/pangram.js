b62f1b1f75d5a255e4056b7d9537a5b9656e0ee4
André Rodrigues <afbr17@gmail.com>
2020-04-04 19:14:36 +0100

    Refactor pangram solution

--- javascript/pangram.js
+++ javascript/pangram.js
@@ -1,23 +1,13 @@
-const createCodes = () => {
-  const alphabetCodes = [];
+const alphabet = 'abcdefghjklmnopqrstuvwxyz';
 
-  for (let i = 97; i < 123; i++) {
-    alphabetCodes.push(i);
-  }
-
-  return alphabetCodes;
-};
-
-export const isPangram = inputString => {
+export const isPangram = (inputString) => {
   inputString = inputString.toLowerCase();
-  let alphabetCodes = createCodes();
 
-  for (const char of inputString) {
-    if (alphabetCodes.includes(char.charCodeAt(0))) {
-      const charIndex = alphabetCodes.indexOf(char.charCodeAt(0));
-      alphabetCodes.splice(charIndex, 1);
+  for (const letter of alphabet) {
+    if (!inputString.includes(letter)) {
+      return false;
     }
   }
 
-  return alphabetCodes.length > 0 ? false : true;
+  return true;
 };
