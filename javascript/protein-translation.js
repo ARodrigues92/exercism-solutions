78d110f75fe467998355d09dec0aae9a6fa95be2
André Rodrigues <arodrigues92@protonmail.ch>
2020-04-07 18:35:47 +0100

    Update protein-translation solution

--- javascript/protein-translation.js
+++ javascript/protein-translation.js
@@ -6,35 +6,38 @@ const codonProteinTable = {
   Tyrosine: ['UAU', 'UAC'],
   Cysteine: ['UGU', 'UGC'],
   Tryptophan: ['UGG'],
-  STOP: ['UAA', 'UAG', 'UGA'],
+};
+
+const stopCodons = ['UAA', 'UAG', 'UGA'];
+
+const translateCodon = (codon) => {
+  for (const protein in codonProteinTable) {
+    if (codonProteinTable[protein].includes(codon)) {
+      return protein;
+    }
+  }
 };
 
 export const translate = (rna) => {
-  let codons = [];
   let proteins = [];
 
-  if (rna) {
-    codons = rna.match(/.{1,3}/g);
-  } else {
+  if (!rna) {
     return proteins;
   }
 
+  const codons = rna.match(/.{1,3}/g);
+
   for (const codon of codons) {
-    for (const protein in codonProteinTable) {
-      if (codonProteinTable[protein].includes(codon)) {
-        if (protein !== 'STOP') {
-          proteins.push(protein);
-          break;
-        } else {
-          return proteins;
-        }
-      }
-      if (protein === 'STOP') {
-        // If it's the last protein and a match wasn't found the codon must be invalid
+    if (!stopCodons.includes(codon)) {
+      const protein = translateCodon(codon);
+      if (protein) {
+        proteins.push(protein);
+      } else {
         throw 'Invalid codon';
       }
+    } else {
+      return proteins;
     }
   }
-
   return proteins;
 };
